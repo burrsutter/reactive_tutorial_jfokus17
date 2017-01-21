674803c0f0362df6770640988421208c4af20da3
Edson Yanaga <edson@yanaga.com.br>
2017-01-21 18:38:52 -0200

    Reformatting and proper encapsulating SocialData

--- 99_Practice/src/main/java/com/burrsutter/reactiveworkshop/SocialData.java
+++ 99_Practice/src/main/java/com/burrsutter/reactiveworkshop/SocialData.java
@@ -1,21 +1,23 @@
 package com.burrsutter.reactiveworkshop;
 
 public class SocialData {
-  public final String id;
-  public final int followers;
-  
-  public SocialData(String id, int followers) {
-     this.id = id;
-     this.followers = followers;      
-  }
 
-  public static SocialData load(String id) {
-    return new SocialData(id, TwitterScreenscraper.getFollowers(id));
-  }
-  @Override
-  public String toString() {
-    return String.format("%s has %d Followers", id, followers);
-  }
+    private final String id;
 
+    private final int followers;
+
+    private SocialData(String id, int followers) {
+        this.id = id;
+        this.followers = followers;
+    }
+
+    public static SocialData load(String id) {
+        return new SocialData(id, TwitterScreenscraper.getFollowers(id));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s has %d Followers", id, followers);
+    }
 
 }
