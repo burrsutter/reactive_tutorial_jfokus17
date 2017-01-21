26e3d76caff6223a0a47291f4f149b6bf4c8150e
Burr Sutter <bsutter@redhat.com>
2017-01-21 18:37:40 -0500

    tweaks

--- 99_Practice/src/main/java/com/burrsutter/reactiveworkshop/Main.java
+++ 99_Practice/src/main/java/com/burrsutter/reactiveworkshop/Main.java
@@ -7,30 +7,5 @@ public class Main {
     public static void main(String[] args) {
         Observable<SocialData> feed = MyObservableServer.getFeed();
         feed.subscribe(System.out::println);
-
-/*
-        feed.subscribe(new Subscriber<SocialData>() {
-        @Override
-        public void onStart() {
-          request(0);
-        }
-
-        @Override
-        public void onCompleted() {
-          System.out.println("completed");
-        }
-
-        @Override 
-        public void onError(Throwable throwable) {
-          System.out.println("Crap! " + throwable);
-          throwable.printStackTrace();
-        }
-
-        @Override
-        public void onNext(SocialData socialData) {
-          System.out.println(socialData);
-        }
-    });
-*/
     }  // main
 } // Main
--- 99_Practice/src/main/java/com/burrsutter/reactiveworkshop/MyObservableServer.java
+++ 99_Practice/src/main/java/com/burrsutter/reactiveworkshop/MyObservableServer.java
@@ -10,13 +10,13 @@ public class MyObservableServer {
     static List<String> ids = Arrays.asList("burrsutter", "yanaga", "realDonaldTrump", "venkat_s");
 
     public static Observable<SocialData> getFeed() {
-        return Observable.create(subscriber -> handleIt(subscriber, ids));
+        return Observable.create(subscriber -> processIt(subscriber, ids));
     }  // getFeed
 
-    public static void handleIt(Subscriber<? super SocialData> subscriber, List<String> ids) {
+    public static void processIt(Subscriber<? super SocialData> subscriber, List<String> ids) {
         ids.stream()
                 .map(SocialData::load)
                 .forEach(subscriber::onNext);
         subscriber.onCompleted();
-    }
+    } // processIt
 }
