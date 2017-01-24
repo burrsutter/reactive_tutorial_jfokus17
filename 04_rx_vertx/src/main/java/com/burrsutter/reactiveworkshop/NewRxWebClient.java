b3b1fba4ba1ee09958839c00148d63e1daa4d6fd
Burr Sutter <bsutter@redhat.com>
2017-01-24 17:18:03 -0500

    added Rx Single

--- 04_rx_vertx/src/main/java/com/burrsutter/reactiveworkshop/NewRxWebClient.java
+++ 04_rx_vertx/src/main/java/com/burrsutter/reactiveworkshop/NewRxWebClient.java
@@ -3,7 +3,7 @@ package com.burrsutter.reactiveworkshop;
 import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.ext.web.client.WebClient;
 import io.vertx.rxjava.ext.web.client.HttpResponse;
-// import rx.Single;
+import rx.Single;
 import io.vertx.rxjava.core.buffer.Buffer;
 
 /**
@@ -16,11 +16,14 @@ public class NewRxWebClient extends AbstractVerticle {
         System.out.println("Stuff");
         WebClient rxWebClient = WebClient.create(vertx);
 
-
+        /*
+           Async Request/Response
+        */
         rxWebClient
                 .get(8080, "localhost", "/api/nap")
                 .timeout(1000)
                 .send(ar -> {
+                    System.out.println("What are YOU: " + ar.getClass().getName());
                     if (ar.succeeded()) {
                         // Obtain response
                         HttpResponse<Buffer> response = ar.result();
@@ -31,5 +34,22 @@ public class NewRxWebClient extends AbstractVerticle {
                         System.out.println("Something went wrong " + ar.cause().getMessage());
                     }
                 });
-    }
+
+        /*
+           Single Response
+        */
+        Single<HttpResponse<Buffer>> single = rxWebClient
+                .get(8080, "localhost", "/api/goodbye")
+                .rxSend();
+
+        // Send a request upon subscription of the Single
+        single.subscribe(response -> {
+            System.out.println("Status: " + response.statusCode());
+            System.out.println("Received: " + response.bodyAsString());
+        }, error -> {
+            System.out.println("Something went wrong " + error.getMessage());
+        }
+        );
+
+    } // start
 }
