299c656f4f7f6be3eb46c989d4d4e3f8ac6595cb
Burr Sutter <bsutter@redhat.com>
2017-02-04 11:06:45 -0500

    adding expense report concept

---
+++ 02-hello-rx/src/main/java/com/burrsutter/reactiveworkshop/Observable3Streams.java
@@ -0,0 +1,30 @@
+package com.burrsutter.reactiveworkshop;
+
+import rx.Observable;
+/**
+ * Created by burr on 2/4/17.
+ */
+public class Observable3Streams {
+    public static void main(String[] args) {
+        Integer[] food = {10, 20, 30};
+        Integer[] hotel = {100, 100, 100};
+        Integer[] beer = {20, 30, 40};
+
+        Observable<Integer> foodStream = Observable.from(food);
+        Observable<Integer> hotelStream = Observable.from(hotel);
+        Observable<Integer> beerStream = Observable.from(beer);
+
+        Observable<Integer> combinedStream = Observable.zip(
+          foodStream, hotelStream, beerStream,
+                (f, h, b) -> {
+                    return f + h + b;
+                }
+        );
+
+        combinedStream.subscribe(
+                System.out::println,
+                Throwable::printStackTrace,
+                () -> System.out.println("COMPLETED")
+        );
+    }
+}
