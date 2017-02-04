f8a5fc545903b1b56958359abc3769f140470280
Burr Sutter <bsutter@redhat.com>
2017-02-04 14:25:01 -0500

    updated .gitignore

--- 02-hello-rx/src/main/java/com/burrsutter/reactiveworkshop/Observable3Streams.java
+++ 02-hello-rx/src/main/java/com/burrsutter/reactiveworkshop/Observable3Streams.java
@@ -6,9 +6,9 @@ import rx.Observable;
  */
 public class Observable3Streams {
     public static void main(String[] args) {
-        Integer[] food = {10, 20, 30};
-        Integer[] hotel = {100, 100, 100};
-        Integer[] beer = {20, 30, 40};
+        Integer[] food =  { 10,  20,  30,  40};
+        Integer[] hotel = {100, 100, 100, 110};
+        Integer[] beer =  { 20,  30,  40,  50};
 
         Observable<Integer> foodStream = Observable.from(food);
         Observable<Integer> hotelStream = Observable.from(hotel);
@@ -17,7 +17,7 @@ public class Observable3Streams {
         Observable<Integer> combinedStream = Observable.zip(
           foodStream, hotelStream, beerStream,
                 (f, h, b) -> {
-                    return f + h + b;
+                    return f + h + (b * 2);
                 }
         );
 
